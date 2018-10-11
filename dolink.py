6d14a895e2446161a760772cacee164c5941084a
Theo Müller <theo.paul.mueller@googlemail.com>
2018-10-11 21:22:24 +0200

    Individual extract

--- dolink.py
+++ dolink.py
@@ -5,12 +5,14 @@ import os
 
 FILENAME = "files.txt"
 
+
 def moveln(name):
     d = os.path.dirname(name)
     if d:
         os.system("mkdir -p {0}".format(d))
     os.system("mv ~/{0} {0}".format(name))
     doln(name)
+
 
 def doln(name):
     d = os.path.dirname(os.path.join("~", name))
@@ -21,15 +23,23 @@ def doln(name):
                                    os.path.join("~", name)))
 
 
+def do_int_ln(name):
+    ny = input("Extract file {}? (y/N) ".format(name))
+    if ny.lower() in ["y", "yes"]:
+        doln(name)
+
+
 def get_list():
     with open(FILENAME, "r") as f:
         return [s.strip() for s in f.readlines()]
+
 
 def add_to_list(names):
     all_names = sorted(list(set(names).union(set(get_list()))))
 
     with open(FILENAME, "w") as f:
         f.writelines(s + "\n" for s in all_names)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 or sys.argv[1] not in ["create", "extract"]:
@@ -38,6 +48,10 @@ if __name__ == "__main__":
     if sys.argv[1] == "extract":
         for name in get_list():
             doln(name)
+
+    elif sys.argv[1] == "extract-int":
+        for name in get_list():
+            do_int_ln(name)
 
     elif sys.argv[1] == "create":
         for item in sys.argv[2:]:
